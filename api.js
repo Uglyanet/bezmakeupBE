bf67814dd93dd45a472921639871668545ed2374
Max Bezvugliak <maximbezvugliak@gmail.com>
2021-03-29 19:27:42 +0300

    Update api

--- api.js
+++ api.js
@@ -42,17 +42,22 @@ router.get("/users", (req, res) => {
 router.post('/latecomer', async (req, res) => {
     const { body } = req;
     const { chat_id } = body
-    const publishedMessages = await Messages.find({ isPublished: true })
-    const sortedPublishedMessages = publishedMessages.sort((a, b) => {
-        if (moment(a.date, 'YYYY MM DD HH:mm').isAfter(moment(b.date, 'YYYY MM DD HH:mm'))) return 1
-        if (moment(b.date, 'YYYY MM DD HH:mm').isAfter(moment(a.date, 'YYYY MM DD HH:mm'))) return -1
-        return 0
-    })
-    for (let index = 0; index < sortedPublishedMessages.length; index++) {
-        const message = sortedPublishedMessages[index]
-        await bot.telegram.sendMessage(chat_id, message.string, {parse_mode:'HTML'});
+    try {
+        const publishedMessages = await Messages.find({ isPublished: true })
+        const sortedPublishedMessages = publishedMessages.sort((a, b) => {
+            if (moment(a.date, 'YYYY MM DD HH:mm').isAfter(moment(b.date, 'YYYY MM DD HH:mm'))) return 1
+            if (moment(b.date, 'YYYY MM DD HH:mm').isAfter(moment(a.date, 'YYYY MM DD HH:mm'))) return -1
+            return 0
+        })
+        for (let index = 0; index < sortedPublishedMessages.length; index++) {
+            const message = sortedPublishedMessages[index]
+            await bot.telegram.sendMessage(chat_id, message.string, { parse_mode: 'HTML' });
+        }
+        res.send(sortedPublishedMessages)
+    } catch (err) {
+        console.error(err)
     }
-    res.send(sortedPublishedMessages)
+
 })
 
 router.post("/users", async (req, res) => {
